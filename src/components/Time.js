33fe70a86db1438b8d599f8eb2ff7ee4a49592f2
pdn-abs <pdn@abstractit.in>
2023-08-10 20:22:01 +0530

    Changed the displaying time format

--- src/components/Time.js
+++ src/components/Time.js
@@ -2,7 +2,8 @@ import { React } from 'react';
 
 const Time = (context) => {
 	const { state: { timeList }, data: { time, id }} = context;
-	const value = timeList[time].toString();
+	// eslint-disable-next-line no-magic-numbers
+	const value = timeList[time].toString().substr(16, 9);
 
 	return (
 		<div key={ id } className="sunTimes">
--- src/core/config.js
+++ src/core/config.js
@@ -1,5 +1,16 @@
 
 const config = {
+	data: [
+		{ place: 'Madurai',
+			latitude: 9.932368732180194,
+			longitude: 78.1238481209068 },
+		{	place: 'Bangalore',
+			latitude: 13.411700690986494,
+			longitude: 77.47565478259357 },
+		{	place: 'Chennai',
+			latitude: 13.069484391471253,
+			longitude: 80.31012742581258 },
+	],
 	timeNameList: ['sunrise', 'sunriseEnd', 'sunsetStart', 'sunset'],
 };
 
